a777ce898777d97acd41a4f609ce7519ce0a4723
fnc314 <fnc314@gmail.com>
2014-01-13 18:09:34 -0800

    first_larger_index - done

--- Lab.rb
+++ Lab.rb
@@ -1,13 +1,16 @@
-Class SortedArra
+class SortedArray
   attr_accessor :internal_arr # This is for convenience in the tests.
 
   def initialize(input_arr=[])
     @internal_arr = []
+    #input_arr.each { |x| add(x) }
     # Fill in the rest of the initialize method here.
     # What should you do with each element of the incoming array?
   end
 
   def add(new_ele)
+    #sort_arr = []
+
     # Hint: Use the Array#insert method.
     raise NotImplementedError.new("You need to implement the add method!")
   end
@@ -17,10 +20,16 @@ Class SortedArra
   end
 
   def [](index)
+    @internal_arr[index]
     raise NotImplementedError.new("You need to implement the [] method!")
   end
 
   def first_larger_index(target, start_ind=0, end_ind=@internal_arr.size)
+    i = start_ind
+    @internal_arr.each { |x| 
+      i += 1 if target > x
+    }
+    return i
     raise NotImplementedError.new("You need to implement the first_larger_index method!")
   end
 
